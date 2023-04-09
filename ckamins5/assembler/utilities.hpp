b1e51a526fbee211ad763a0b4c714bfe9e63980d
ckamins5 <ckamins5@89f2d086-3cf4-4f20-903d-6ebdc21123ae>
2023-04-09 21:57:13 +0000

    worked on P3 M2 and LAB 10 has been done since last week but forgot to add into the commit

---
+++ ckamins5/assembler/utilities.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include "../string/string.hpp"
+#include "stack.hpp"
+#include <fstream>
+
+void inFixToPostfix(std::ostream*, std::ifstream&);
+
+
+#endif //UTILITIES_HPP
