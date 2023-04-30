75e37f85e84b1a044b11157ee55c6ee833a07ae8
ckamins5 <ckamins5@89f2d086-3cf4-4f20-903d-6ebdc21123ae>
2023-04-30 22:08:25 +0000

    forgot some testing_2 files

---
+++ ckamins5/testing_2/test_default_ctor.cpp
@@ -0,0 +1,29 @@
+//  String class test program
+//
+//  Name:  Cole Kaminski
+//  Tests: Default constructor
+//
+
+#include "string.hpp"
+#include <cassert>
+#include <iostream>
+
+//===========================================================================
+int main()
+{
+        {
+                //------------------------------------------------------
+                // SETUP FIXTURE
+                String  left;
+             //   std::cout<<left<<std::endl;
+                assert((left == '\0'));
+           //     assert(left.length() == 0);
+
+
+
+        }
+
+std::cout<< "Default constructor done testing\n";
+
+
+}
